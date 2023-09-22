e4a02b12f11cd8a027f4723998df8afa4be6aeea
Taras <ivanyuk.taras@gmail.com>
2023-09-22 07:51:35 -0500

    Create send_password_reset mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
     field :sign_in_user, mutation: Mutations::SignInUser
+    field :send_password_reset, mutation: Mutations::SendPasswordReset
     # field :sign_out_user, mutation: Mutations::SignOutUser
   end
 end
