bfb6a87847272fdc1da5623aa7cef0b9078ec17d
Taras <ivanyuk.taras@gmail.com>
2023-10-04 08:16:00 -0500

    Create composition union

---
+++ app/graphql/types/composition_union.rb
@@ -0,0 +1,20 @@
+module Types
+  class CompositionUnion < Types::BaseUnion
+    possible_types Types::BookType, Types::MovieType, Types::Music::AlbumType, Types::Music::SongType
+
+    def self.resolve_type(obj, _ctx)
+      case obj
+      when Book
+        Types::BookType
+      when Movie
+        Types::MovieType
+      when Album
+        Types::Music::AlbumType
+      when Song
+        Types::Music::SongType
+      else
+        raise "Unexpected object: #{obj}"
+      end
+    end
+  end
+end
